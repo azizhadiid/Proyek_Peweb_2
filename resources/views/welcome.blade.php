8b4da909bdc776d1838f2766ea1ae38bff51dd3a
zuldhiya <ddzulfaqor09@gmail.com>
2025-05-06 10:49:44 +0700

    banner

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -267,7 +267,7 @@
                   <div class="swiper-wrapper">
                     
                     <div class="swiper-slide">
-                      <div class="row banner-content p-5">
+                      <div class="row banner-content p-5 align-items-center">
                         <div class="content-wrapper col-md-7">
                           <div class="categories my-3">100% Asli Nanas</div>
                           <h3 class="display-4">Vinas</h3>
@@ -281,29 +281,29 @@
                     </div>
                     
                     <div class="swiper-slide">
-                      <div class="row banner-content p-5">
+                      <div class="row banner-content p-5 align-items-center">
                         <div class="content-wrapper col-md-7">
-                          <div class="categories mb-3 pb-3">100% natural</div>
-                          <h3 class="banner-title">Fresh Smoothie & Summer Juice</h3>
-                          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Dignissim massa diam elementum.</p>
-                          <a href="#" class="btn btn-outline-dark btn-lg text-uppercase fs-6 rounded-1">Shop Collection</a>
+                          <div class="categories mb-3 pb-3">100% Asli Nanas</div>
+                          <h3 class="banner-title">Nanas Crispy</h3>
+                          <p class="fs-5">Camilan sehat berbahan dasar nanas pilihan yang dikeringkan secara alami untuk mempertahankan rasa manis aslinya.</p>
+                          <a href="#" class="btn btn-outline-dark btn-lg text-uppercase fs-6 rounded-1">Belanja Sekarang!</a>
                         </div>
                         <div class="img-wrapper col-md-5">
-                          <img src="{{ asset('assets-user/images/product-thumb-1.png') }}" class="img-fluid">
+                          <img src="{{ asset('assets-user/images/snack.png') }}" class="img-fluid" style="max-width: 350px; height: 600px;">
                         </div>
                       </div>
                     </div>
                     
                     <div class="swiper-slide">
-                      <div class="row banner-content p-5">
+                      <div class="row banner-content p-5 align-items-center">
                         <div class="content-wrapper col-md-7">
-                          <div class="categories mb-3 pb-3">100% natural</div>
-                          <h3 class="banner-title">Heinz Tomato Ketchup</h3>
-                          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Dignissim massa diam elementum.</p>
-                          <a href="#" class="btn btn-outline-dark btn-lg text-uppercase fs-6 rounded-1">Shop Collection</a>
+                          <div class="categories mb-3 pb-3">100% Asli Nanas</div>
+                          <h3 class="banner-title">Dodol Nanas</h3>
+                          <p class="fs-5">Camilan manis kenyal berbahan nanas asli yang menghadirkan cita rasa tropis khas dalam setiap gigitan.</p>
+                          <a href="#" class="btn btn-outline-dark btn-lg text-uppercase fs-6 rounded-1">Belanja Sekarang!</a>
                         </div>
                         <div class="img-wrapper col-md-5">
-                          <img src="{{ asset('assets-user/images/product-thumb-2.png') }}" class="img-fluid">
+                          <img src="{{ asset('assets-user/images/dodol.png') }}" class="img-fluid">
                         </div>
                       </div>
                     </div>
@@ -314,25 +314,23 @@
                 </div>
               </div>
               
-              <div class="banner-ad bg-success-subtle block-2" style="background:url('{{ asset('assets-user/images/ad-image-1.png') }} ') no-repeat;background-position: right bottom">
+              <div class="banner-ad bg-success-subtle block-2" style="background:url('{{ asset('assets-user/images/keripik.png') }} ') no-repeat;background-position: right bottom">
                 <div class="row banner-content p-5">
-
                   <div class="content-wrapper col-md-7">
-                    <div class="categories sale mb-3 pb-3">20% off</div>
-                    <h3 class="banner-title">Fruits & Vegetables</h3>
-                    <a href="#" class="d-flex align-items-center nav-link">Shop Collection <svg width="24" height="24"><use xlink:href="#arrow-right"></use></svg></a>
+                    <div class="categories mb-3 pb-3">Keripik Nanas Madu</div>
+                    <h3 class="banner-title">Rasakan kenikmatannya!</h3>
+                    <a href="#" class="d-flex align-items-center nav-link">Belanja Sekarang!<svg width="30" height="30"><use xlink:href="#arrow-right"></use></svg></a>
                   </div>
-
                 </div>
               </div>
 
-              <div class="banner-ad bg-danger block-3" style="background:url('{{ asset('assets-user/images/ad-image-2.png') }} ') no-repeat;background-position: right bottom">
+              <div class="banner-ad bg-danger block-3" style="background:url('{{ asset('assets-user/images/jelly.png') }} ') no-repeat;background-position: right bottom">
                 <div class="row banner-content p-5">
 
                   <div class="content-wrapper col-md-7">
-                    <div class="categories sale mb-3 pb-3">15% off</div>
-                    <h3 class="item-title">Baked Products</h3>
-                    <a href="#" class="d-flex align-items-center nav-link">Shop Collection <svg width="24" height="24"><use xlink:href="#arrow-right"></use></svg></a>
+                    <div class="categories mb-3 pb-3">Jelly Nanas Madu</div>
+                    <h3 class="item-title">Rasakan Kelembutannya!</h3>
+                    <a href="#" class="d-flex align-items-center nav-link">Belanja Sekarang!<svg width="30" height="30"><use xlink:href="#arrow-right"></use></svg></a>
                   </div>
 
                 </div>
