41ca4217fa577f7d6fcb9d8b8ec7d415d946693c
azizhadiid <azizalhadiid88@gmail.com>
2025-05-06 21:36:07 +0700

    Coba CRUD Artikal

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,13 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\BarangController;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+
+// Khusus Admin
+Route::get('/admin/barang/buat', [BarangController::class, 'create'])->name('barang.create');
+Route::post('/admin/barang/simpan', [BarangController::class, 'store'])->name('barang.store');
