253f5a9e7b6d6a79d875bd3655712f4b083e44f9
fanjiz <irpanaziz232@gmail.com>
2025-05-08 14:09:34 +0700

    desain login dan regis

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,69 +1,68 @@
 <!DOCTYPE html>
 <html lang="en">
-    <head>
-        <meta charset="utf-8" />
-        <meta http-equiv="X-UA-Compatible" content="IE=edge" />
-        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
-        <meta name="description" content="" />
-        <meta name="author" content="" />
-        <title>Login - SB Admin</title>
-        <link href="{{asset('assets-admin/css/styles.css')}}" rel="stylesheet" />
-        <script src="https://use.fontawesome.com/releases/v6.3.0/js/all.js" crossorigin="anonymous"></script>
+<head>
+	<meta charset="utf-8">
+	<meta name="author" content="Muhamad Nauval Azhar">
+	<meta name="viewport" content="width=device-width,initial-scale=1">
+	<meta name="description" content="This is a login page template based on Bootstrap 5">
+	<title>Login RasaTangkit</title>
+    <link href="{{asset('assets-admin/css/styles.css')}}" rel="stylesheet" />
     </head>
-    <body class="bg-primary">
-        <div id="layoutAuthentication">
-            <div id="layoutAuthentication_content">
-                <main>
-                    <div class="container">
-                        <div class="row justify-content-center">
-                            <div class="col-lg-5">
-                                <div class="card shadow-lg border-0 rounded-lg mt-5">
-                                    <div class="card-header"><h3 class="text-center font-weight-light my-4">Login</h3></div>
-                                    <div class="card-body">
-                                        <form>
-                                            <div class="form-floating mb-3">
-                                                <input class="form-control" id="inputEmail" type="email" placeholder="name@example.com" />
-                                                <label for="inputEmail">Email address</label>
-                                            </div>
-                                            <div class="form-floating mb-3">
-                                                <input class="form-control" id="inputPassword" type="password" placeholder="Password" />
-                                                <label for="inputPassword">Password</label>
-                                            </div>
-                                            <div class="form-check mb-3">
-                                                <input class="form-check-input" id="inputRememberPassword" type="checkbox" value="" />
-                                                <label class="form-check-label" for="inputRememberPassword">Remember Password</label>
-                                            </div>
-                                            <div class="d-flex align-items-center justify-content-between mt-4 mb-0">
-                                                <a class="small" href="password.html">Forgot Password?</a>
-                                                <a class="btn btn-primary" href="index.html">Login</a>
-                                            </div>
-                                        </form>
-                                    </div>
-                                    <div class="card-footer text-center py-3">
-                                        <div class="small"><a href="/register">Need an account? Sign up!</a></div>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </main>
-            </div>
-            <div id="layoutAuthentication_footer">
-                <footer class="py-4 bg-light mt-auto">
-                    <div class="container-fluid px-4">
-                        <div class="d-flex align-items-center justify-content-between small">
-                            <div class="text-muted">Copyright &copy; Your Website 2023</div>
-                            <div>
-                                <a href="#">Privacy Policy</a>
-                                &middot;
-                                <a href="#">Terms &amp; Conditions</a>
-                            </div>
-                        </div>
-                    </div>
-                </footer>
-            </div>
-        </div>
-        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" crossorigin="anonymous"></script>
-        <script src="{{asset('assets-admin/js/scripts.js')}}"></script>
-    </body>
+
+<body style="background-color: #FFB22C;">
+	<section class="h-100">
+		<div class="container h-100">
+			<div class="row justify-content-sm-center h-100">
+				<div class="col-xxl-4 col-xl-5 col-lg-5 col-md-7 col-sm-9">
+					<div class="text-center my-5">
+						<img src="{{ asset('assets-user/images/logo.png') }}" alt="logo" width="300">
+					</div>
+					<div class="card shadow-lg">
+						<div class="card-body p-5">
+							<h1 class="fs-4 card-title fw-bold mb-4">Masuk Ke Akunmu</h1>
+							<form method="POST" class="needs-validation" novalidate="" autocomplete="off">
+								<div class="mb-3">
+									<label class="mb-2 text-muted" for="email">E-Mail</label>
+									<input id="email" type="email" class="form-control" name="email" value="" required autofocus>
+									<div class="invalid-feedback">
+										Email tidak valid
+									</div>
+								</div>
+
+								<div class="mb-3">
+									<div class="mb-2 w-100">
+										<label class="text-muted" for="password">Password</label>
+									</div>
+									<input id="password" type="password" class="form-control" name="password" required>
+								    <div class="invalid-feedback">
+								    	Password dibutuhkan
+							    	</div>
+								</div>
+
+								<div class="d-flex align-items-center mt-4" >
+                                        <a href="forgot.html" class="float-end">
+											Forgot Password?
+										</a>
+									<button type="submit" class="btn btn-primary ms-auto" href="dashboard">
+										Masuk
+									</button>
+								</div>
+							</form>
+						</div>  
+						<div class="card-footer py-3 border-0">
+							<div class="text-center">
+								Belum punya akun? <a href="register" class="text-dark">Buat akun</a>
+							</div>
+						</div>
+					</div>
+					<div class="text-center mt-5 text-muted">
+						Copyright &copy; 2025 &mdash; AHID Production 
+					</div>
+				</div>
+			</div>
+		</div>
+	</section>
+
+	<script src="js/login.js"></script>
+</body>
 </html>
--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,117 +1,85 @@
 <!DOCTYPE html>
 <html lang="en">
-    <head>
-        <meta charset="utf-8" />
-        <meta http-equiv="X-UA-Compatible" content="IE=edge" />
-        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
-        <meta name="description" content="" />
-        <meta name="author" content="" />
-        <title>Register - SB Admin</title>
-        <link href="{{asset('assets-admin/css/styles.css')}}" rel="stylesheet" />
-        <script src="https://use.fontawesome.com/releases/v6.3.0/js/all.js" crossorigin="anonymous"></script>
+<head>
+	<meta charset="utf-8">
+	<meta name="author" content="Muhamad Nauval Azhar">
+	<meta name="viewport" content="width=device-width,initial-scale=1">
+	<meta name="description" content="This is a login page template based on Bootstrap 5">
+	<title>Register RasaTangkit</title>
+    <link href="{{asset('assets-admin/css/styles.css')}}" rel="stylesheet" />
     </head>
-    <body class="bg-primary">
-        <div id="layoutAuthentication">
-            <div id="layoutAuthentication_content">
-                <main>
-                    <div class="container">
-                        <div class="row justify-content-center">
-                            <div class="col-lg-7">
-                                <div class="card shadow-lg border-0 rounded-lg mt-5">
-                                    <div class="card-header"><h3 class="text-center font-weight-light my-4">Create Account</h3></div>
-                                    <div class="card-body">
-                                        @if ($errors->any())
-                                        <div class="alert alert-danger mt-3 alert-dismissible fade show" role="alert" style="width: 100%">
-                                            <div class="d-flex align-items-center">
-                                                <i class="bi bi-exclamation-circle-fill me-2"></i>
-                                                <div>
-                                                    @foreach ($errors->all() as $error)
-                                                    <p class="m-0">{{ $error }}</p>
-                                                    @endforeach
-                                                </div>
-                                            </div>
-                                            <button type="button" class="btn-close" data-bs-dismiss="alert"
-                                                aria-label="Close"></button>
-                                        </div>
-                                        @endif
 
-                                        {{-- Jika Sukses Login --}}
-                                        @if (session('success'))
-                                        <div class="alert alert-success" style="width: 100%">
-                                            {{ session('success') }}
-                                        </div>
-                                        @endif
+<body style="background-color: #FFB22C;">
+	<section class="h-100">
+		<div class="container h-100">
+			<div class="row justify-content-sm-center h-100">
+				<div class="col-xxl-4 col-xl-5 col-lg-5 col-md-7 col-sm-9">
+					<div class="text-center my-5">
+						<img src="{{ asset('assets-user/images/logo.png') }}" alt="logo" width="300">
+					</div>
+					<div class="card shadow-lg">
+						<div class="card-body p-5">
+							<h1 class="fs-4 card-title fw-bold mb-4">Buat Akun Baru</h1>
+							<form method="POST" class="needs-validation" novalidate="" autocomplete="off">
+                                <div class="mb-3">
+									<div class="mb-2 w-100">
+										<label class="text-muted" for="username">Username</label>
+									</div>
+									<input id="username" type="username" class="form-control" name="username" required autofocus>
+								    <div class="invalid-feedback">
+								    	Username tidak valid
+							    	</div>
+								</div>
 
-                                        {{-- jika Password telah di ubah --}}
-                                        @if (session('status'))
-                                        <div class="alert alert-success" style="width: 100%">
-                                            {{ session('status') }}
-                                        </div>
-                                        @endif
-                                        <form method="POST" action="{{url('/register/create')}}" class="mt-2">
-                                            @csrf
-                                            <div class="row gy-3 gy-md-4 overflow-hidden">
-                                                <div class="col-12">
-                                                    <label for="nama" class="form-label">Nama <span
-                                                            class="text-danger">*</span></label>
-                                                    <input type="nama" class="form-control" name="nama" id="nama"
-                                                        placeholder="Jhond Doe" required>
-                                                </div>
-            
-                                                <div class="col-12">
-                                                    <label for="email" class="form-label">Email <span
-                                                            class="text-danger">*</span></label>
-                                                    <input type="email" class="form-control" name="email" id="email"
-                                                        placeholder="name@example.com" required>
-                                                </div>
-                                                <div class="col-12">
-                                                    <label for="password" class="form-label">Password <span
-                                                            class="text-danger">*</span></label>
-                                                    <input type="password" class="form-control" name="password" id="password"
-                                                        value="" required placeholder="********">
-                                                </div>
-            
-                                                <div class="col-12">
-                                                    <label for="password_confirmation" class="form-label">Confirm Password <span
-                                                            class="text-danger">*</span></label>
-                                                    <input type="password" class="form-control" name="password_confirmation" id="password_confirmation"
-                                                        value="" required placeholder="********">
-                                                        
-                                                </div>
-                                                <hr class="border-secondary-subtle">
-                                                <div class="col-12">
-                                                    <div class="d-grid">
-                                                        <button class="btn btn-primary" type="submit">Register Now</button>
-                                                    </div>
-                                                </div>
-                                            </div>
-                                        </form>
-                                    </div>
-                                    <div class="card-footer text-center py-3">
-                                        <div class="small"><a href="/login">Have an account? Go to login</a></div>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </main>
-            </div>
-            <div id="layoutAuthentication_footer">
-                <footer class="py-4 bg-light mt-auto">
-                    <div class="container-fluid px-4">
-                        <div class="d-flex align-items-center justify-content-between small">
-                            <div class="text-muted">Copyright &copy; Your Website 2023</div>
-                            <div>
-                                <a href="#">Privacy Policy</a>
-                                &middot;
-                                <a href="#">Terms &amp; Conditions</a>
-                            </div>
-                        </div>
-                    </div>
-                </footer>
-            </div>
-        </div>
-        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" crossorigin="anonymous"></script>
-        <script src="{{asset('assets-admin/js/scripts.js')}}"></script>
-    </body>
+								<div class="mb-3">
+									<label class="mb-2 text-muted" for="email">E-Mail</label>
+									<input id="email" type="email" class="form-control" name="email" value="" required>
+									<div class="invalid-feedback">
+										Email tidak valid
+									</div>
+								</div>
+
+								<div class="mb-3">
+									<div class="mb-2 w-100">
+										<label class="text-muted" for="password">Password</label>
+									</div>
+									<input id="password" type="password" class="form-control" name="password" required>
+								    <div class="invalid-feedback">
+								    	Password dibutuhkan
+							    	</div>
+								</div>
+
+                                <div class="mb-3">
+									<div class="mb-2 w-100">
+										<label class="text-muted" for="password">Konfir Password</label>
+									</div>
+									<input id="password" type="password" class="form-control" name="password" required>
+								    <div class="invalid-feedback">
+								    	Konfirmasi password
+							    	</div>
+								</div>
+
+								<div class="d-flex align-items-center mt-4">
+									<button type="submit" class="btn btn-primary ms-auto">
+										Buat Akun
+									</button>
+								</div>
+							</form>
+						</div>
+						<div class="card-footer py-3 border-0">
+							<div class="text-center">
+								Sudah punya akun? <a href="login" class="text-dark">Masuk ke akunmu</a>
+							</div>
+						</div>
+					</div>
+					<div class="text-center mt-5 text-muted">
+						Copyright &copy; 2025 &mdash; AHID Production 
+					</div>
+				</div>
+			</div>
+		</div>
+	</section>
+
+	<script src="js/login.js"></script>
+</body>
 </html>
