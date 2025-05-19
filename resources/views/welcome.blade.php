4fa928d27b0fefdf6c3916aa7241fdf29915dec9
hilgit01 <pchilmyai@gmail.com>
2025-05-19 09:28:52 +0700

    19/5

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -145,31 +145,24 @@
           <div class="col-sm-4 col-lg-3 text-center text-sm-start">
             <div class="main-logo">
               <a href="index.html">
-                <img src="{{ asset('assets-user/images/logo.png') }}" alt="logo" class="img-fluid">
+              <img src="{{ asset('assets-user/images/logo_rasa_tangkit.png') }}" alt="logo" class="img-fluid custom-logo-size">
               </a>
             </div>
           </div>
           
-          <div class="col-sm-6 offset-sm-2 offset-md-0 col-lg-5 d-none d-lg-block">
-            <div class="search-bar row bg-light p-2 my-2 rounded-4">
-              <div class="col-md-4 d-none d-md-block">
-                <select class="form-select border-0 bg-transparent">
-                  <option>All Categories</option>
-                  <option>Groceries</option>
-                  <option>Drinks</option>
-                  <option>Chocolates</option>
-                </select>
+          <div class="col-lg-5 d-none d-lg-block">
+            <form id="search-form" class="text-center" action="index.html" method="post">
+              <div class="input-group bg-light p-2 my-2 rounded-4">
+                <input type="text" class="form-control border-0 bg-transparent" placeholder="Search for more than 20,000 products" />
+                <button class="btn bg-transparent border-0" type="submit">
+                  <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24">
+                    <path fill="currentColor" d="M21.71 20.29L18 16.61A9 9 0 1 0 16.61 18l3.68 3.68a1 1 0 0 0 1.42 0a1 1 0 0 0 0-1.39ZM11 18a7 7 0 1 1 7-7a7 7 0 0 1-7 7Z"/>
+                  </svg>
+                </button>
               </div>
-              <div class="col-11 col-md-7">
-                <form id="search-form" class="text-center" action="index.html" method="post">
-                  <input type="text" class="form-control border-0 bg-transparent" placeholder="Search for more than 20,000 products" />
-                </form>
-              </div>
-              <div class="col-1">
-                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path fill="currentColor" d="M21.71 20.29L18 16.61A9 9 0 1 0 16.61 18l3.68 3.68a1 1 0 0 0 1.42 0a1 1 0 0 0 0-1.39ZM11 18a7 7 0 1 1 7-7a7 7 0 0 1-7 7Z"/></svg>
-              </div>
-            </div>
+            </form>
           </div>
+
           
           <div class="col-sm-8 col-lg-4 d-flex justify-content-end gap-5 align-items-center mt-4 mt-sm-0 justify-content-center justify-content-sm-end">
             <div class="support-box text-end d-none d-xl-block">
@@ -200,17 +193,15 @@
               </li>
             </ul>
 
-            <div class="cart text-end d-none d-lg-block dropdown">
-              <button class="border-0 bg-transparent d-flex flex-column gap-2 lh-1" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasCart" aria-controls="offcanvasCart">
-                <span class="fs-6 text-muted dropdown-toggle">Your Cart</span>
-                <span class="cart-total fs-5 fw-bold">$1290.00</span>
-              </button>
-            </div>
+          <div class="cart text-end d-none d-lg-block dropdown">
+            <button class="border-0 bg-transparent d-flex flex-column gap-2 lh-1" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasCart" aria-controls="offcanvasCart">
+              <span class="fs-6 text-muted dropdown-toggle">Your Cart</span>
+              <span class="cart-total fs-5 fw-bold">$1290.00</span>
+            </button>
           </div>
-
         </div>
-      </div>
-      <div class="container-fluid">
+
+        <div class="container-fluid">
         <div class="row py-3">
           <div class="d-flex  justify-content-center justify-content-sm-between align-items-center">
             <nav class="main-menu d-flex navbar navbar-expand-lg">
@@ -277,11 +268,13 @@
                   </ul>
                 
                 </div>
-
               </div>
-          </div>
+            </div>
+           </div>
         </div>
       </div>
+      </div>
+
     </header>
     
     <section class="py-3" style="background-image: url('images/background-pattern.jpg');background-repeat: no-repeat;background-size: cover;">
